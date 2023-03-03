99f3bfb74d456df0dbd3977ba271274b27874504
Sergio <you@example.com>
2023-03-03 18:26:42 +0100

    Ejemplo de datos

--- Curso_Python/holaMundo.py
+++ Curso_Python/holaMundo.py
@@ -1,1 +1,9 @@
-print('Hola mundo')
+print("Estos son los datos de seguridad ")
+nombre = input("Escriba su nombre: ")
+edad = input("Introduzca su edad: ")
+localidad = input("Escriba su localidad: ")
+
+print("Sus datos son: ")
+print(nombre)
+print(edad)
+print(localidad)
